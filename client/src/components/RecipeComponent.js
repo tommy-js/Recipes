b9feb03ab243c6e124f4fd293f8118e92a9507cc
tommy-js <tylerb814925@hotmail.com>
2020-04-17 15:05:56 -0400

    UI modifications

--- client/src/components/RecipeComponent.js
+++ client/src/components/RecipeComponent.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "../app.scss";
+import like from "../icons/like.png";
+import like_filled from "../icons/like_filled.png";
+import share from "../icons/share.png";
+import share_filled from "../icons/share_filled.png";
 
 function RecipeComponent(props) {
+  const [likeSelected, setLikeSelected] = useState(35);
+  const [shareSelected, setShareSelected] = useState(35);
+  const [liked, setLiked] = useState(false);
+  const [shared, setShared] = useState(false);
+
+  function highlightLike() {
+    if (!liked) {
+      setLikeSelected(0);
+    } else {
+      setLikeSelected(35);
+    }
+    setLiked(!liked);
+  }
+
+  function highlightShare() {
+    if (!shared) {
+      setShareSelected(0);
+    } else {
+      setShareSelected(35);
+    }
+    setShared(!shared);
+  }
+
+  function mouseoverLike() {
+    if (!liked) {
+      setLikeSelected(0);
+    }
+  }
+
+  function mouseoutLike() {
+    if (!liked) {
+      setLikeSelected(35);
+    }
+  }
+
+  function mouseoverShare() {
+    if (!shared) {
+      setShareSelected(0);
+    }
+  }
+
+  function mouseoutShare() {
+    if (!shared) {
+      setShareSelected(35);
+    }
+  }
+
   return (
     <div className="recipe_component">
       <div className="recipe_image_container">
@@ -11,6 +62,35 @@ function RecipeComponent(props) {
         />
       </div>
       <h2 className="recipe_header">{props.name}</h2>
+      <div className="recipe_interaction_bar">
+        <div
+          className="button_item"
+          onMouseOver={mouseoverLike}
+          onMouseOut={mouseoutLike}
+          onClick={highlightLike}
+        >
+          <img className="like_button" src={like} />
+          <div
+            className="background_white"
+            style={{ height: likeSelected + "px" }}
+          ></div>
+          <img className="like_button" src={like_filled} />
+        </div>
+        <div
+          className="button_item"
+          onMouseOver={mouseoverShare}
+          onMouseOut={mouseoutShare}
+          onClick={highlightShare}
+        >
+          <img className="share_button" src={share} />
+          <div
+            className="background_white"
+            style={{ height: shareSelected + "px" }}
+          ></div>
+          <img className="share_button" src={share_filled} />
+        </div>
+      </div>
+      <h3 className="recipe_header">Directions</h3>
       <p className="recipe_content">{props.content}</p>
     </div>
   );
